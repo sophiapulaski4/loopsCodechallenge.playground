21f67e51a42c7638305943c2bc28a312b5395729
caroline dugan <caroline.dugan@gmail.com>
2019-07-11 09:50:42 -0700

    Initial Commit

---
+++ Contents.swift
@@ -0,0 +1,98 @@
+import UIKit
+
+import UIKit
+//We are given arrays of scholar names, hobbies, and ages
+//This is a playground that will print what each scholar's name, age, and hobby is.
+var scholarNames = [
+    "Aileen",
+    "Aishiki",
+    "Ameera",
+    "Cady",
+    "Ellen",
+    "Elora",
+    "Fiona",
+    "Halle",
+    "Hunter",
+    "Jamie",
+    "Kyla",
+    "Maira",
+    "Maya",
+    "Michelle",
+    "Nailah",
+    "Natalie",
+    "Nicoletta",
+    "Pegah",
+    "Sabyatha",
+    "Sophia"
+]
+var scholarAges = [
+    17, //Aileen
+    14, //Aishiki
+    14, //Ameera
+    13, //Cady
+    17, //Ellen
+    15, //Elora
+    14, //Fiona
+    16, //Halle
+    17, //Hunter
+    15, //Jamie
+    15, //Kyla
+    15, //Maira
+    15, //Maya
+    16, //Michelle
+    15, //Nailah
+    16, //Natalie
+    15, //Nicoletta
+    18, //Pegah
+    17, //Sabyatha
+    14 //Sophia
+]
+var scholarHobbies = [
+    "making Youtube videos", //Aileen
+    "listening to musical theather", //Aishiki
+    "running", //Ameera
+    "ballet", //Cady
+    "likes playing with her dogs", //Ellen
+    "playing the Sims", //Elora
+    "playing flute", //Fiona
+    "playing golf", //Halle
+    "likes dancing", //Hunter
+    "painting", //Jamie
+    "reading", //Kyla
+    "playing with motion graphics", //Maira
+    "reading", //Maya
+    "sleeping", //Michelle
+    "reading fan fiction", //Nailah
+    "playing soccer", //Natalie
+    "reading", //Nicolatta
+    "drawing", //Pegah
+    "likes crocheting", //Sabyatha
+    "swimming" //Sophia
+]
+//Write loop challenge code here!
+
+
+//for pair in scholars {
+//    print(pair.key)
+//}
+
+var name = ["Chloe", "Maya"]
+
+var age = [18]
+
+var hobby = ["Photography", "Contortion"]
+
+//print(name[0], age[0], hobby[0])
+
+for index in 0..<name.count{
+    print("", name, age, hobby [index])
+
+}
+
+//Extension: How can I write a loop so it prints only the information of scholars who are 15-years-old? 16-years-old?
+
+
+
+
+
+
